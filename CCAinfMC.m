4941f7b886091efd2f4b103500abd8be5138cd91
nicholst <thomas.nichols@bdi.ox.ac.uk>
2019-08-26 06:26:51 +0100

    Exact version Anderson used

--- CCAinfMC.m
+++ CCAinfMC.m
@@ -23,20 +23,12 @@ function CCAinfMC(N,Ny,Nx,Nz,Npca,nR,nP,HuhJhun)
 
 FreemanLane=false;
 
-fprintf('Simulation parameters: ')
-fprintf('N: %d, Ny: %d, Nx: %d, Nz: %d, Npca: %d, nR: %d, nP: %d, HJ: %d\n',...
-    N,Ny,Nx,Nz,Npca,nR,nP,HuhJhun);
-
 % For each realization:
 for r = 1:nR
-    fprintf('Realization %d:\n',r)
+    fprintf('Realization %d\n',r)
     
     % Create some random data. Use rng for repeatability:
-    if isoctave
-        rand('state','reset'); %#ok
-    else
-        rng('shuffle');
-    end
+    rng('shuffle');
     Y = randn(N,Ny);
     X = randn(N,Nx);
     Z = [randn(N,Nz-1) ones(N,1)];
@@ -45,6 +37,11 @@ for r = 1:nR
     pZ = pinv(Z);
     Rz = eye(N) - Z*pZ;
     if HuhJhun
+        % % Tom's version
+        % [Q,S]  = svd(Rz);
+        % S      = diag(S) < 10*eps;
+        % Q(:,S) = [];
+        
         % Huh Juhn's / Anderson's version
         [Q,D]  = schur(Rz);
         D      = abs(diag(D)) < 10*eps;
@@ -62,12 +59,15 @@ for r = 1:nR
     end
     
     % CCA:
-    R = cca(Y,X);
-
+    [A,B,R,U,V,stats] = canoncorr(Y,X);
+    
     % Permutation test:
-    pcorr  = ones(size(R));
+    pperm = ones(size(R));
+    pcorr = ones(size(R));
     for p = 1:(nP-1)
         if r == 1 && p == 1
+            ppararep  = zeros(nR,length(R));  % Parametric p-value from CCA F-test
+            ppermrep  = zeros(nR,length(R));  % Permutation p-value based on F-test
             pcorrrep  = zeros(nR,length(R));  % Permutation p-value based on r
             corrFirst = zeros(nR,length(R));  % To store the CCs for the first permutation
             corrLast  = corrFirst;            % To store the CCs for the last permutation
@@ -77,44 +77,29 @@ for r = 1:nR
         end
         if FreemanLane
             tmpX = X(randperm(size(X,1)),:);
-            Rp = cca(Y,tmpX-Z*(pZ*tmpX));
+            [~,~,Rp,~,~,statsp] = canoncorr(Y,tmpX-Z*(pZ*tmpX));
         else
-            Rp = cca(Y,X(randperm(size(X,1)),:));
+            [~,~,Rp,~,~,statsp] = canoncorr(Y,X(randperm(size(X,1)),:));
         end
-        pcorr  = pcorr  + (Rp  >= R);
+        pperm = pperm + (statsp.F >= stats.F);
+        pcorr = pcorr + (Rp >= R);
     end
-    pcorr  = pcorr/nP;
+    pperm = pperm/nP;
+    pcorr = pcorr/nP;
+    ppararep (r,:) = stats.pF;
+    ppermrep (r,:) = pperm;
     pcorrrep (r,:) = pcorr;
     corrFirst(r,:) = R;
     corrLast (r,:) = Rp;
-    fprintf('- P-values (permutation [corr]):'); disp(pcorr);
-    fprintf('- CCs (not permuted):');  disp(R);
-    fprintf('- CCs (random perm):'); disp(Rp);
 end
 
+fprintf('Simulation parameters:\n')
+fprintf('N: %d, Ny: %d, Nx: %d, Nz: %d, Npca: %d, nR: %d, nP: %d, HJ: %d\n',...
+    N,Ny,Nx,Nz,Npca,nR,nP,HuhJhun);
 alpha = 0.05;
 fprintf('Results:\n')
-fprintf('FPR (permutation [corr]):');  disp(mean(pcorrrep <= alpha));
-fprintf('Mean CCs (not permuted):');  disp(mean(corrFirst));
-fprintf('Mean CCs (random perm):'); disp(mean(corrLast));
-
-fprintf('AWK(FPR-P,FPR-pF,FPR-pr,mr,mpr): %g %g %g %g %g\n',...
-	[ppararep(:,1),...
-	 ppermrep(:,1),...
-	 pcorrrep(:,1),...
-	 corrFirst(:,1),...
-	 corrLast(:,1)]');
-
-% =================================================================
-function cc = cca(X,Y)
-[Qx,~,~] = qr(X,0);
-[Qy,~,~] = qr(Y,0);
-cc = svds(Qx'*Qy,1);
-
-% =================================================================
-function y = isoctave
-persistent isoct;
-if isempty(isoct),
-    isoct = exist('OCTAVE_VERSION','builtin') ~= 0;
-end
-y = isoct;
+fprintf('FPR (parametric):\n');      disp(mean(ppararep<=alpha));
+fprintf('FPR (permutation [F]):\n'); disp(mean(ppermrep<=alpha));
+fprintf('FPR (permutation [r]):\n'); disp(mean(pcorrrep<=alpha));
+fprintf('Mean CCs (unpermuted)\n');  disp(mean(corrFirst));
+fprintf('Mean CCs (random perm)\n'); disp(mean(corrLast));
