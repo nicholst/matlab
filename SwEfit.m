ced68e2358417a8c9b8b8430d21d9d4892ade272
nicholst <thomas.nichols@bdi.ox.ac.uk>
2021-03-27 17:47:23 +0000

    Integrated Vg computation

--- SwEfit.m
+++ SwEfit.m
@@ -1,5 +1,5 @@
-function [cbetahat, cbetaSE, Vg_out] = SwEfit(X,bID,Y,con,Vg)
-% FUNCTION [cbetahat, cbetaSE, Vg_out] = SwEfit(X,bID,Y[,con,Vg])
+function [cbetahat, cbetaSE, Vg] = SwEfit(X,bID,Y,con,Vg)
+% FUNCTION [cbetahat, cbetaSE, Vg] = SwEfit(X,bID,Y[,con,Vg])
 %
 % Estimate betahat with OLS and obtain standard errors using the 'classic'
 % Sandwich estimator, 
@@ -7,13 +7,13 @@ function [cbetahat, cbetaSE, Vg_out] = SwEfit(X,bID,Y,con,Vg)
 %   X   - Design matrix, N x P
 %   bID - Block IDs, identifying clusters, might be family, site, subject (for repeated mes) 
 %   Y   - Data, N x Nelm
-%   con - Matrix of t contrasts, Ncon x P; if omitted defaults to eye(P)
-%   Vg  - Global working covariance; a cell array such that inv(Vg{i}) is an 
-%         estimate of the covariance of the data for block i. If omitted, independence
-%         working covariance is used. If is a non-zero scalar, is estimated
+%   con - Matrix of t contrasts, Ncon x P; if omitted or empty defaults to eye(P)
+%   Vg  - Global working covariance:
+%             []  - use independence (default)
+%             1   - estimate the global covariance on the fly, otw
+%             Vg  - cell array such that inv(Vg{i}) is the working covariance for block i.
 %
-%   Vg_out  - Global averaged covariance; an estimate suitable for re-calling function in a 
-%         second pass.
+%   Vg  - Global working covariance; useful if Vg is computed on the fly.
 %
 % T. Nichols 24 March 2021
 % See https://github.com/nicholst/matlab/blob/master/LICENSE
@@ -32,9 +32,11 @@ if ~all(size(bID)==[N,1])
 end
 IDs    = unique(bID(:));
 Nblock = length(IDs);
-bNs    = zeros(1,Nblock);
+bI     = cell(Nblock,1);
+bN     = zeros(1,Nblock);
 for i = 1:Nblock
-    bNs(i) = sum(IDs(i)==bID);
+    bI{i} = find(IDs(i)==bID);
+    bN(i) = length(bI{i});
 end
 Nelm   = size(Y,2);
 if size(Y,1)~=N
@@ -48,27 +50,44 @@ else
     end
 end
 Ncon = size(con,1);
+CalcVg = false;
 if nargin < 5
-    Vg = {};
+    Vg = [];
 else
-    if ~iscell(Vg) || length(Vg)~=Nblock
-        error('Global variance not a cell array with an matrix per block');
-    end
-    for i = 1:Nblock
-        I    = find(IDs(i)==bID);
-        Ns   = length(I);
-        if ~all(size(Vg{i})==[Ns Ns])
-            error(sprintf('Global variance for block %d wrong size',i));
+    if ~iscell(Vg) 
+        if ~isempty(Vg) && prod(size(Vg))~=1
+            error('Global variance Vg not a scalar nor a cell array with an matrix per block')
         end
-        W{i} = inv(Vg{i});
+        if Vg
+            CalcVg = true;
+        end
+    else
+        if length(Vg)~=Nblock
+            error('Global variance Vg not a scalar nor a cell array with an matrix per block');
+        end
+        for i = 1:Nblock
+            if ~all(size(Vg{i})==[bN(i) bN(i)])
+                error(sprintf('Global variance for block %d wrong size',i));
+            end
+            W{i} = inv(Vg{i});
+        end
     end
-end
-if nargout>2 
-    Vg_out = cell(Nblock,1);
-else
-    Vg_out = cell(0);
 end
 
+
+%
+% Compute global working covariance, if needed
+%
+if CalcVg
+    pX  = pinv(X);
+    res = Y-X*pX*Y;
+    for i = 1:Nblock
+        I    = bI{i};
+        Ra   = sqrtm(inv(eye(bN(i))-X(bI{i},:)*pX(:,I)));
+        ares = Ra*res(I,:);
+        Vg{i} = ares*ares'/Nelm;
+    end
+end
 
 %
 % Compute BreadXW = inv(X'*W*X)*X'*W, used to estimate bh and S
@@ -80,10 +99,7 @@ else
     XtWX = zeros(P,P);
     XtW  = zeros(P,N);
     for i = 1:Nblock
-        I    = find(IDs(i)==bID);
-        if ~all(size(Vg{i})==[bNs(i) bNs(i)])
-            error(sprintf('Global variance for block %d wrong size',i));
-        end
+        I        = bI{i};
         W{i}     = inv(Vg{i});
         XtWX     = XtWX + X(I,:)'*W{i}*X(I,:);
         XtW(:,I) = X(I,:)'*W{i};
@@ -99,8 +115,8 @@ end
 %
 Ra = cell(1,Nblock);
 for i = 1:Nblock
-    I     = find(IDs(i)==bID);
-    Ra{i} = sqrtm(inv(eye(bNs(i))-X(I,:)*BreadXW(:,I)));
+    I     = bI{i};
+    Ra{i} = sqrtm(inv(eye(bN(i))-X(I,:)*BreadXW(:,I)));
 end
 
 %
@@ -110,21 +126,19 @@ end
 bh   = BreadXW*Y;
 res  = Y-X*bh;
 
+
 %
 % Compute robust (sandwich) variance estimator
 %
 S      = zeros(P,P,Nelm);
 for i = 1:Nblock
-    I    = find(IDs(i)==bID);
+    I    = bI{i};
     % BreadXW times half of Meat, with type 1 residual correction
     ares = Ra{i}*res(I,:);
     E    = BreadXW(:,I)*ares;
     E    = reshape(E,[P,1,Nelm]);
     % Full `Bread*Meat*Bread' contribution for block b
     S    = S + pagemtimes(E,'none',E,'transpose');
-    if length(Vg_out)>0
-        Vg_out{i} = ares*ares'/Nelm;
-    end
 end
 
 %
