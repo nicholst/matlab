98ee4f5e646d41b87133d937f494a81ad3bcf006
nicholst <thomas.nichols@bdi.ox.ac.uk>
2021-11-07 16:19:55 +0000

    Latest updates

--- SwEfitdemo.m
+++ SwEfitdemo.m
@@ -9,15 +9,15 @@
 % 'block' is cluster variable, might be family, site, subject (for repeated mes)
 Nblock    = 1000;
 Nperblock = 4;       % Number of observations pers block
-Nelm      = 1000;    % Number of vertices/voxels
+Nelm      = 2000;    % Number of vertices/voxels
 rho       = 0.95;    % Intrablock correlation... maxed out to verify SwE is working
 
 P         = 10;      % Number of predictors... all fake/simulated
 N         = Nblock*Nperblock;
 Iblock    = repelem([1:Nblock]',Nperblock);
-prop0     = 0.95;     % proportion of predictor that piles up at min value
+prop0     = 0.95;    % proportion of predictor that piles up at min value
 alph      = 0.05;    % Nominal alpha for FPR report
-nWB       = 999;    % Number of Wild Bootstrap iterations
+nWB       = 100;    % Number of Wild Bootstrap iterations - Start at 100; try 1000
 
 % Design: intercept, one between block variable, rest within block
 myrand=@(p,q,prop0)[rand(p,q).*binornd(1,1-prop0,p,q)];
@@ -64,64 +64,66 @@ tic;
 [cbetahat1,cbetaSE1,Vg]=SwEfit(X,Iblock,Y,[],1);
 fprintf('SwE vectorised, global W: ');toc
 
+Tswe0 = cbetahat0./cbetaSE0;
+Tswe1 = cbetahat1./cbetaSE1;
 
 if nWB>0
     tic
     % Wild Bootstrap
-    Pwb0 = zeros(P,Nelm); 
-    Pwb1 = zeros(P,Nelm);
     res0 = Y-X*cbetahat0;
     res1 = Y-X*cbetahat1;
+    Pwb0 = zeros(P,Nelm); 
+    Pwb1 = zeros(P,Nelm);
     for i=1:nWB
         if rem(i,10)==0; fprintf('%d ',i); end
         WBf=kron(2*binornd(1,0.5,Nblock,1)-1,ones(Nperblock,1));
-        Ywb0 = X*cbetahat0 + WBf.*res0;
-        Pwb0 = Pwb0 + (SwEfit0(X,Iblock,Ywb0) >= cbetahat0);
-        Ywb1 = X*cbetahat1 + WBf.*res1;
-        Pwb1 = Pwb1 + (SwEfit(X,Iblock,Ywb1,[],1) >= cbetahat1);
+
+        Ywb0 = WBf.*res0;
+        [cbwb,cbSEwb] = SwEfit0(X,Iblock,Ywb0);
+        Pwb0 = Pwb0 + (cbwb./cbSEwb >= Tswe0);
+
+        Ywb0 = WBf.*res1;
+        [cbwb,cbSEwb] = SwEfit(X,Iblock,Ywb0,[],1);
+        Pwb1 = Pwb1 + (cbwb./cbSEwb >= Tswe1);
     end
     Pwb0 = (Pwb0+1)/(nWB+1);
     Pwb1 = (Pwb1+1)/(nWB+1);
     fprintf('\nSwE (both) Wild Bootstrap: ');toc
 end    
 
-Tswe0 = cbetahat0./cbetaSE0;
-Tswe1 = cbetahat1./cbetaSE1;
+cname={'Mean','PureBtwCov','PureWtnCov','BtwWtnCov'};
 
 fprintf('\nEfficiency of non-iid working cov relative to OLS\n')
-fprintf('PureBtwCov: SD(beta_swe1)/SD(beta_ols) = %f\n',...
-        std(cbetahat1(2,:))/std(bh(2,:)));
-fprintf('PureWtnCov: SD(beta_swe1)/SD(beta_ols) = %f\n',...
-        std(cbetahat1(3,:))/std(bh(3,:)));
-fprintf('BtwWtnCov:  SD(beta_swe1)/SD(beta_ols) = %f\n',...
-        std(cbetahat1(4,:))/std(bh(4,:)));
+for i = 2:4
+    fprintf('%12s: SD(beta_swe1)/SD(beta_ols) = %f\n',...
+        cname{i},std(cbetahat1(i,:))/std(bh(i,:)));
+end
 
 fprintf('\nStandard deviation of T (should be 1.0)\n')
-fprintf('PureBtwCov: SD(T_ols) = %f  SD(T_swe0) = %f  SD(T_swe1) = %f\n',...
-        std(Tols(2,:)), std(Tswe0(2,:)),  std(Tswe1(2,:)));
-fprintf('PureWtnCov: SD(T_ols) = %f  SD(T_swe0) = %f  SD(T_swe1) = %f\n',...
-        std(Tols(3,:)), std(Tswe0(3,:)),  std(Tswe1(3,:)));
-fprintf('BtwWtnCov:  SD(T_ols) = %f  SD(T_swe0) = %f  SD(T_swe1) = %f\n',...
-        std(Tols(4,:)), std(Tswe0(4,:)),  std(Tswe1(4,:)));
+for i = 2:4
+    fprintf('%12s: SD(T_ols) = %f  SD(T_swe0) = %f  SD(T_swe1) = %f\n',...
+        cname{i},std(Tols(i,:)), std(Tswe0(i,:)),  std(Tswe1(i,:)));
+end
 
 Ta=tinv(1-alph,N-P);
 fprintf('\nFPR (nominal %g, CI [%.4f,%.4f])\n',alph,alph+[-1,1]*sqrt(alph*(1-alph)/Nelm));
-fprintf('PureBtwCov: FPR(T_ols) = %f  FPR(T_swe0) = %f  FPR(T_swe1) = %f  FPR(T_swe1wb) = %f\n',...
-        mean(Tols(2,:)>=Ta), mean(Tswe0(2,:)>=Ta),  mean(Tswe1(2,:)>=Ta),  mean(Pwb1(2,:)<=alph));
-fprintf('PureWtnCov: FPR(T_ols) = %f  FPR(T_swe0) = %f  FPR(T_swe1) = %f  FPR(T_swe1wb) = %f\n',...
-        mean(Tols(3,:)>=Ta), mean(Tswe0(3,:)>=Ta),  mean(Tswe1(3,:)>=Ta),  mean(Pwb1(2,:)<=alph));
-fprintf('BtwWtnCov:  FPR(T_ols) = %f  FPR(T_swe0) = %f  FPR(T_swe1) = %f  FPR(T_swe1wb) = %f\n',...
-        mean(Tols(4,:)>=Ta), mean(Tswe0(4,:)>=Ta),  mean(Tswe1(4,:)>=Ta),  mean(Pwb1(2,:)<=alph));
+for i = 2:4
+    fprintf('%12s: T_ols: %.3f  T_swe0: %.3f  T_swe0wb: %.3f  T_swe1: %.3f  T_swe1wb: %.3f\n',...
+        cname{i},mean(Tols(i,:)>=Ta),mean(Tswe0(i,:)>=Ta),mean(Pwb0(i,:)<=alph),mean(Tswe1(i,:)>=Ta),mean(Pwb1(i,:)<=alph));
+end
+
+figure;imagesc(X)
 
 Str={'Pure between','Pure within','Mixed'};
 for i = 2:4
-    figure(i)
+    figure
     P0 = tcdf(Tswe0(i,:),N-P,'upper');
     P1 = tcdf(Tswe1(i,:),N-P,'upper');
-    loglog((1:Nelm)'/Nelm,[sort(P0'),sort(P1')]);
-    grid on;legend('swe0','swe1','AutoUpdate','off')
+    loglog((1:Nelm)'/Nelm,[sort(P0'),sort(P1'),sort(Pwb0(i,:)'),sort(Pwb1(i,:)')],'linewidth',2);
+    grid on;legend('swe0','swe1','sweWB0','sweWB1','AutoUpdate','off')
     set(refline(1),'linestyle',':')
     title(sprintf("Parameter %d: %s",i,Str{i-1}))
 end
 
-figure(5);nhist({Tswe0(4,:),Tswe1(4,:)});title('Parameter 4');legend({'swe0','swe1'})
+
+                                       
