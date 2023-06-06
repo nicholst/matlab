4f02d1c7927d150e03179beb62f3c1becd44d457
nicholst <thomas.nichols@bdi.ox.ac.uk>
2023-06-06 13:41:02 +0100

    Add BKY adaptive FDR method

--- FDR.m
+++ FDR.m
@@ -1,11 +1,14 @@
-function [pID,pN] = FDR(p,aFDR)
-% FORMAT [pID,pN] = FDR(p,aFDR)
+function [pID,pN,pA] = FDR(p,aFDR)
+% FORMAT [pID,pN,pA] = FDR(p,aFDR)
 % 
 % p     - vector of p-values
 % aFDR  - alpha_FDR, aka q, the desired False Discovery Rate level
 %
 % pID - p-value threshold based on independence or positive dependence
 % pN  - Nonparametric p-value threshold
+% pA  - Adaptive FDR result of Benjamini, Krieger, Yekutieli, 2006. Adaptive 
+%       linear step-up procedures that control the false discovery rate. 
+%       Biometrika 93, 491–507. doi:10.1093/biomet/93. 
 %______________________________________________________________________________
 % Author: T. Nichols
 % Version: http://github.com/nicholst/matlab/tree/$Format:%h$
@@ -19,15 +22,25 @@ I = (1:V)';
 
 cVID = 1;
 cVN = sum(1./(1:V));
+denomVA = V + 1 - I*(1-aFDR);
 
-pID = p(max(find(p<=I/V*aFDR/cVID)));
+
+pID = p(find(p<=I/V*aFDR/cVID,1,'last'));
 if isempty(pID)
   pID=0;
 end
 
-pN = p(max(find(p<=I/V*aFDR/cVN)));
+pN = p(find(p<=I/V*aFDR/cVN,1,'last'));
 if isempty(pN)
   pN=0; 
 end
 
-
+% This is non-looping implimentation of the BKY method... It certainly
+% doesn't reject too much, but need to double-check I'm not missing any
+% rejections.
+tmp = find(p>I*aFDR/denomVA,1,'first');
+tmp = max(tmp-1,1);
+pA = p(tmp);
+if isempty(pA)
+  pA=0;
+end
